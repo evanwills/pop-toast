7043c4ecc394629cc09e9b995b45bd44cb027126
Evan Wills <evan.wills@acu.edu.au>
2022-10-07 19:11:27 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,380 @@
+# `<POP-TOAST>`
+
+`<pop-toast>` is used to show a cancelable message in an unobtrusive
+manner. If, like me, you don't know what a toast component is, read
+this post by [Chris Coyier](https://css-tricks.com/toast/)
+
+I have created this web component using [lit](https://lit.dev/), leaning on the great work done by
+[Bootstrap](https://getbootstrap.com/docs/5.2/components/toasts/),
+with extra functionality inspired by the
+[University of Sydney's Library site's](https://www.library.sydney.edu.au/) use of a toast component to inform users about cultural issues the site might pose.
+
+It (hopefully) has sensible defaults that can easily be overridden either with attributes for behaviour or [CSS Custom Properties](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties) for styling
+
+## Attributes
+
+### `position`
+
+Position on the page of the toast will be
+
+Options are:
+* `top-left` - Positioned at the __top, left__ of the window
+* `top-centre` - Positioned at the __top, centre__ of the window
+* `top-right` - Positioned at the __top, right__ of the window
+* `middle-left` - Positioned at the __middle, left__ of the window
+* `middle-centre` - Positioned at __exact centre__ of the window
+* `middle-right` - Positioned at the __middle, right__ of the window
+* `bottom-left` - Positioned at the __bottom, left__ of the window
+* `bottom-centre` - Positioned at the __bottom, centre__ of the window
+* `bottom-right` - Positioned at the __bottom, right__ of the window
+
+If empty, it will default to the being inline where the HTML is
+rendered. This can be overridden by setting CSS Custom Properties
+via an external style sheet.
+
+### `hidedxpanded`
+
+Normally, when the close button is clicked the toast just vanishes.
+When the page is reloaded or the same toast is rendered on a
+different page, it will show as normal. If `hideonreload` is included
+as an attribute in `<pop-toast>`, an entry is added to local storage
+to prevent a toast with the same ID from being rendered after reload
+or when loaded on a different page.
+
+> __Note:__ If the user's browser doesn't support local storage or
+>           the user has blocked local storage, this will have no
+>           effect.
+
+### `hideonreload`
+
+Normally, when the close button is clicked the toast just vanishes.
+If `hideonreload` is TRUE, an entry is added to local storage to
+prevent a toast with the same ID from being rendered after reload
+or when loaded on a different page.
+
+> __Note:__ For `hideonreload` to work, the `<pop-toast>` element
+>           must have an ID
+
+### `moretxt`
+
+Override the text shown in the read more button.<br />
+[Default: "_More..._"]
+
+### `label`
+
+If there is a `label` attribute on the custom element, that title
+will be rendered as the label for the component.
+
+### `closeChar`
+
+Override the character(s) shown in the close button.<br />
+[Default: "_X_"]
+
+### `id`
+
+If the toast has an ID attribute and `hideOnReload` is included as
+an attribute in `<pop-toast>`, then when the close button is clicked
+an item is added to the user's browser local storage so the next time
+a `<pop-toast>` component with the same ID is included in the HTML on
+a page, it will __*NOT*__ be rendered
+
+-----
+
+## Slots
+
+For those of you who don't know what `slots` are used to add content
+from the pages HTML within a web component.
+
+Content within each slot will be styled like it would be elsewhere
+on the page.
+
+`<pop-toast>` has three named slots
+
+### `head`
+
+Heading for the toast content.
+
+### `teaser`
+
+Teaser content for the tost.
+(Usually a single paragraph to draw people in)
+
+### `body`
+
+Extra content that is hidden by default when the toast is rendered
+but can be shown by clicking on the 'more...' button.
+
+```html
+<pop-toast label="Stale bread???" id="pop-toast" position="bottom-left">
+    <div slot="head">
+        <h1>Why toast is useful</h1>
+    </div>
+    <div slot="teaser">
+        <p>
+            Toast is a good way to use bread that is stale. (It makes
+            it you feel like you intended it to be stale. Rather than
+            being stuck with bread you'd rather chuck out.)
+        </p>
+    </div>
+    <div slot="body">
+        <p>
+            There are many things you can do with toast such as:
+        </p>
+        <ul>
+            <li>Use it to squash cockroaches or flys in your home</li>
+            <li>Throw it like a frisbee at your annoying sibling
+                while your parents are arguing about pointless
+                things.</li>
+            <li>Use a lighter to burn images of your chosen deity
+                and sell it on ebay</li>
+            <li>Slather it with peanut button and honey and leave
+                it on the table to catch flys</li>
+            <li>If you are really desperate you could even eat it.</li>
+        </ul>
+        <p>
+            Learn more about
+            <a href="https://css-tricks.com/toast/">toast</a>.
+        </p>
+    </div>
+</pop-toast>
+```
+
+-----
+
+## Styling
+
+Most of the content for `<pop-toast>` will inherit styling from the
+page it is rendered on but it's not unusual for developers and
+designers to want some level of control over the styling of the
+component itself. To achieve this there are a number of
+[CSS Custom Properties](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties)
+that can be set via the pages style sheet.
+
+You can also use media queries to override any of the custom properties at break points you prefer
+
+### `--toast-border`
+
+Used to style the border around the toast component.<br />
+[Default: `0.05rem solid #fff`]
+
+### `--toast-bottom`
+
+Offset of the component from the bottom of the window (when `fixed`) or the bottom of the nearest positioned element (when `absolute`).<br />
+[Default: `auto`]
+
+### `--toast-height`
+
+Height of toast component<br />
+[Default: `auto`]
+
+### `--toast-left`
+
+Offset of the component from the left side of the window (when `fixed`) or the left side of the nearest positioned element (when `absolute`).<br />
+[Default: `50%`]
+
+### `--toast-overflow`
+
+Overflow control for toast content area<br />
+[Default: `auto`]
+
+
+### `--toast-padding`
+
+Padding for toast's wrapping div<br />
+[Default: `1rem`]
+
+### `--toast-position`
+
+How the whole toast component is positioned.<br />
+[Default: `fixed`]
+
+### `--toast-right`
+
+Offset of the component from the right side of the window (when `fixed`) or the right side of the nearest positioned element (when `absolute`).<br />
+[Default: `auto`]
+
+### `--toast-shadow`
+
+Defines the styling of the box shadow for the whole toast component.<br />
+[Default: `0 0 0.5rem rgba(255, 255, 255, 0.5)`]
+
+Font weight of the toast's title block<br />
+[Default: `bold`]
+
+### `--toast-top`
+
+Offset of the component from the top of the window (when `fixed`) or the top of the nearest positioned element (when `absolute`).<br />
+[Default: `50%`]
+
+### `--toast-translate-x`
+
+How to transform/translate the toast component in the __X__ axis<br />
+[Default: `0`]
+
+
+### `--toast-translate-y`
+
+How to transform/translate the toast component in the __Y__ axis<br />
+[Default: `0`]
+
+
+### `--toast-width`
+
+Controls the width of the toast component<br />
+[Default: `calc(100% - 2rem)`]
+
+-----
+
+### Close button
+
+#### `--toast-close-bg-colour`
+
+Background colour for close (X) button<br />
+[Default: `transparent`]
+
+#### `--toast-close-border`
+
+Border styling for close button.<br />
+[Default: `none`]
+
+#### `--toast-close-colour`
+
+Font colour for close button<br />
+[Default: `inherit`]
+
+#### `--toast-close-line-height`
+
+Line height of text in close button<br />
+[Default: `1rem`]
+
+#### `--toast-close-padding`
+
+Padding for close button<br />
+[Default: `0.25rem`]
+
+#### `--toast-close-right`
+
+Right side offset for close button<br />
+[Default: `0`]
+
+#### `--toast-close-top`
+
+Top offset for close button<br />
+[Default: `-0.3rem`]
+
+-----
+
+### Toast label
+
+#### `--toast-label-bg-colour`
+
+Background colour for toast's label<br />
+[Default: `inherit`]
+
+#### `--toast-label-border`
+
+Styling for border of toast's label<br />
+[Default: `none`]
+
+#### `--toast-label-case`
+
+How to transform the text in the toast's label<br />
+[Default: `uppercase`]
+
+#### `--toast-label-decoration`
+
+What sort of text decoration is used for toast's label<br />
+[Default: `none`]
+
+#### `--toast-label-family`
+
+Font family for toast's label<br />
+[Default: `inherit`]
+
+#### `--toast-label-line-height`
+
+Line height for toast's label<br />
+[Default: `1rem`]
+
+#### `--toast-label-margin`
+
+Toast label's margin<br />
+[Default: `0`]
+
+#### `--toast-label-padding`
+
+Toast label's padding<br />
+[Default: `0 2rem 0 0`]
+
+#### `--toast-label-size`
+
+Font size for toast's label<br />
+[Default: `0.875rem`]
+
+#### `--toast-label-style`
+
+Font stying for toast's label<br />
+[Default: `normal`]
+
+#### `--toast-label-weight`
+
+Font weight for toast's label<br />
+[Default: `bold`]
+
+-----
+
+### Read more/expand button
+#### `--toast-more-align`
+
+Text alignment for _more..._ button<br />
+[Default: `right`]
+
+#### `--toast-more-bg-colour`
+
+Background colour for _more..._ button<br />
+[Default: `inherit`]
+
+#### `--toast-more-border`
+
+Border styling for  for _more..._ button<br />
+[Default: `none`]
+
+#### `--toast-more-colour`
+
+Text colour for _more..._ button<br />
+[Default: `inherit`]
+
+#### `--toast-more-decoration`
+
+Text decoration for _more..._ button<br />
+[Default: `none`]
+
+#### `--toast-more-margin`
+
+_more..._ button's margins<br />
+[Default: `-2.1rem 0 0 0`]
+
+#### `--toast-more-padding`
+
+_more..._ button's padding<br />
+[Default: `0`]
+
+#### `--toast-more-family`
+
+Font family for _more..._ button<br />
+[Default: `inherit`]
+
+#### `--toast-more-size`
+
+Font size for _more..._ button<br />
+[Default: `0.875rem`]
+
+#### `--toast-more-style`
+
+Font style for _more..._ button<br />
+[Default: `normal`]
+
+#### `--toast-more-weight`
+
+Font weight for _more..._ button<br />
+[Default: `normal`]
---
+++ src/pop-toast.ts
@@ -0,0 +1,419 @@
+import { LitElement, css, html, TemplateResult } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+/**
+ * A toast component
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('pop-toast')
+export class MyElement extends LitElement {
+  /**
+   * Position on the page of the toast will be
+   *
+   * If empty, it will default to the being inline where the HTML is
+   * rendered. This can be overridden by setting CSS Custom
+   * Properties via an external style sheet.
+   */
+  @property({ type: String })
+  position : string = '';
+
+  /**
+   * Normally when the "more..." button is clicked (when toast has
+   * extra body) the body is shown and nothing else happens. But if
+   * `hideExpanded` is TRUE, when the "more..." button is clicked
+   * but the toast isn't closed, next time a toast with the same ID
+   * is loaded, it will be hidden.
+   */
+  @property({ type: Boolean})
+  hideExpanded : boolean = false;
+
+  /**
+   * Normally, when the close button is clicked the toast just
+   * vanishes. If `hideOnReload` is TRUE, an entry is added to local
+   * storage to prevent a toast with the same ID from being rendered
+   * after reload or when loaded on a different page.
+   */
+  @property({ type: Boolean })
+  hideOnReload : boolean = false;
+
+  /**
+   * Text shown in the read more button
+   */
+  @property({ type: String })
+  moreTxt : string = 'More...';
+
+  /**
+   * Text shown in the read more button
+   */
+  @property({ type: String })
+  label : string = '';
+
+  /**
+   * Character(s) shown in the close button
+   */
+  @property({ type: String })
+  closeChar : string = 'X';
+
+  /**
+   * Whether or not the toast is expanded
+   *
+   * (Only relevant if toast has extra body content)
+   */
+  @state()
+  expanded : boolean = false;
+
+  /**
+   * Whether or not the toast is closed
+   */
+  @state()
+  closed : boolean = false;
+
+  /**
+   * Whether or not the init method has been called
+   */
+  private _init : boolean = false;
+
+  /**
+   * Local storage key
+   *
+   * Used to check if
+   */
+  private _lsKey : string = '';
+
+  /**
+   * Whether or not toast has extra body content
+   */
+  private _hasBody: boolean = false;
+
+  static styles = css`
+    :host {
+      --toast-border: 0.05rem solid #fff;
+      --toast-bottom: auto;
+      --toast-close-bg-colour: transparent;
+      --toast-close-border: none;
+      --toast-close-colour: inherit;
+      --toast-close-line-height: 1rem;
+      --toast-close-padding: 0.25rem;
+      --toast-close-right: 0;
+      --toast-close-top: -0.3rem;
+      --toast-height: auto;
+      --toast-label-bg-colour: inherit;
+      --toast-label-border: none;
+      --toast-label-case: uppercase;
+      --toast-label-decoration: none;
+      --toast-label-family: inherit;
+      --toast-label-line-height: 1rem;
+      --toast-label-margin: 0;
+      --toast-label-padding: 0 2rem 0 0;
+      --toast-label-size: 0.875rem;
+      --toast-label-style: normal;
+      --toast-label-weight: bold;
+      --toast-left: 50%;
+      --toast-more-max-width: calc(100% - 2rem);
+      --toast-more-align: right;
+      --toast-more-bg-colour: inherit;
+      --toast-more-border: none;
+      --toast-more-colour: inherit;
+      --toast-more-decoration: none;
+      --toast-more-family: inherit;
+      --toast-more-margin: -2.1rem 0 0 0;
+      --toast-more-padding: 0;
+      --toast-more-size: 0.875rem;
+      --toast-more-style: normal;
+      --toast-more-weight: normal;
+      --toast-overflow: auto;
+      --toast-padding: 1rem;
+      --toast-position: fixed;
+      --toast-right: auto;
+      --toast-shadow: 0 0 0.5rem rgba(255, 255, 255, 0.5);
+      --toast-top: 50%;
+      --toast-translate-x: 0;
+      --toast-translate-y: 0;
+      --toast-width: calc(100% - 2rem);
+    }
+    .closed {
+      display: none;
+    }
+    .toast {
+      border: var(--toast-border);
+      box-shadow: var(--toast-shadow);
+      box-sizing: border-box;
+      height: var(--toast-height);
+      left:  var(--toast-left);
+      max-width: var(--toast-max-width);
+      padding: var(--toast-padding);
+      position: var(--toast-position);
+      top:  var(--toast-top);
+      width: var(--toast-width);
+      transform: translate(var(--toast-translate-x), var(--toast-translate-y));
+    }
+    .toast.top-right {
+      bottom: auto;
+      left: auto;
+      right:  var(--toast-right);
+      top:  var(--toast-top);
+    }
+    .toast.top-centre {
+      bottom: auto;
+      left: 50%;
+      right: auto;
+      top:  var(--toast-top);
+      transform: translateX(-50%);
+    }
+    .toast.top-left {
+      bottom: auto;
+      left:  var(--toast-left);
+      right: auto;
+      top:  var(--toast-top);
+    }
+    .toast.middle-right {
+      bottom: auto;
+      left: auto;
+      right:  var(--toast-right);
+      top:  50%;
+      transform: translateY(-50%);
+    }
+    .toast.middle-centre {
+      bottom: auto;
+      left: 50%;
+      top:  50%;
+      right:  auto;
+      transform: translate(-50%, -50%);
+    }
+    .toast.middle-left {
+      bottom: auto;
+      left:  var(--toast-left);
+      right: auto;
+      top:  50%;
+      transform: translateY(-50%);
+    }
+    .toast.bottom-right {
+      bottom:  var(--toast-bottom);
+      left: auto;
+      right:  var(--toast-right);
+      top: auto;
+    }
+    .toast.bottom-centre {
+      bottom:  var(--toast-bottom);
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    .toast.bottom-left {
+      bottom:  var(--toast-bottom);
+      left:  var(--toast-left);
+      right: auto;
+      top: auto;
+    }
+    .toast-header {
+      position: relative;
+    }
+    .toast-body{
+      overflow-y: var(--toast-overflow, auto);
+    }
+    .label {
+      border: var(--toast-label-border);
+      background-color: var(--toast-label-bg-colour);
+      margin: var(--toast-label-margin);
+      padding: var(--toast-label-padding);
+      font-family: var(--toast-label-family);
+      font-size: var(--toast-label-size);
+      font-style: var(--toast-label-style);
+      font-weight: var(--toast-label-weight);
+      line-height: var(--toast-label-line-height);
+      text-transform: var(--toast-label-case);
+      text-decoration: var(--toast-label-decoration);
+    }
+    .close {
+      display: inline-block;
+      background-color: var(--toast-close-bg-colour);
+      border: var(--toast-close-border)
+      color: var(--toast-close-colour);
+      cursor: pointer;
+      font-family: var(--toast-close-family);
+      font-size: var(--toast-close-size);
+      font-weight: var(--toast-close-weight);
+      line-height: var(--toast-close-line-height);
+      margin: var(--toast-close-margin);
+      padding: var(--toast-close-padding);
+      position: absolute;
+      top: var(--toast-close-top);
+      right: var(--toast-close-right);
+    }
+    .more {
+      background-color: var(--toast-more-bg-colour);
+      border: var(--toast-more-border);
+      color: var(--toast-more-colour);
+      cursor: pointer;
+      display: block;
+      font-family: var(--toast-more-family);
+      font-size: var(--toast-more-size);
+      font-style: var(--toast-more-style);
+      font-weight: var(--toast-more-weight);
+      margin: var(--toast-more-margin);
+      padding: var(--toast-more-padding);
+      text-align: var(--toast-more-align);
+      text-decoration: var(--toast-more-decoration);
+      width: 100%;
+    }
+  `;
+
+  /**
+   * Handle close toast click
+   */
+  private _clickClose() {
+    this.closed = !this.closed;
+    this._setClosed();
+  }
+
+  /**
+   * Handle expand body click
+   */
+  private _clickExpand() {
+    this.expanded = !this.expanded;
+    if (this.hideExpanded === true) {
+      this._setClosed();
+    }
+  }
+
+  /**
+   * Set the local storage so
+   */
+  private _setClosed() {
+    if (this._lsKey !== '') {
+      window.localStorage.setItem(this._lsKey, 'closed');
+    }
+  }
+
+  /**
+   * Do some basic initialisation stuff the first time the component
+   * is rendered
+   */
+  private _doInit() : void {
+    if (this._init === false) {
+      this._init = true;
+
+      console.log('this:', this)
+
+      // Try and find a standard position
+      if (this.position !== '') {
+        let original = this.position.toLowerCase();
+        this.position = '';
+        let tmp = '';
+
+        if (original.includes('bot')) {
+          tmp = 'bottom';
+        } else if (original.includes('top')) {
+          tmp = 'top'
+        } else if (original.includes('mid')) {
+          tmp = 'middle'
+        }
+
+        if (original.includes('lef')) {
+          tmp += '-left';
+        } else if (original.includes('rig')) {
+          tmp += '-right'
+        } else if (original.includes('cent')) {
+          tmp += '-centre'
+        }
+
+        if (tmp.match(/^(?:top|middle|bottom)-(?:left|centre|right)$/)) {
+          this.position = ' ' + tmp;
+        }
+      }
+
+      // Whether or not to persist the closed state for toasts with
+      // the same ID
+      if (this.hideOnReload === true && this.id !== '' && this._storageAvailable()) {
+        this._lsKey = this.id.trim().
+                              replace(/[^a-z0-9_-]+/ig, '-').
+                              replace(/^-|-$/g, '');
+        this._lsKey = 'popToast.' + this._lsKey;
+        const tmp = window.localStorage.getItem(this._lsKey);
+        if (tmp !== null && tmp === 'closed') {
+          this.closed = true;
+        }
+      }
+
+      // Check whether or not the `body` slot is empty.
+      const bodySlot = this.querySelector('*[slot="body"]')
+      this._hasBody = (bodySlot !== null && bodySlot.innerHTML.trim() !== '');
+    }
+  }
+
+  /**
+   * Test whether or not the browser has local storage enabled
+   *
+   * This is taken from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+   *
+   * @returns TRUE if localStorage is available. FALSE otherwise
+   */
+  private _storageAvailable() : boolean {
+    let storage;
+    try {
+      if (typeof window.localStorage !== 'undefined') {
+        storage = window.localStorage;
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+      } else {
+        return false;
+      }
+    }
+    catch (e) {
+      return (e instanceof DOMException && (
+        // everything except Firefox
+        e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        (storage && storage.length !== 0));
+    }
+  }
+
+
+  render() : TemplateResult {
+    this._doInit();
+    const tab = (this.closed)
+      ? '-1'
+      : undefined;
+
+    return html`
+      <div class="toast${this.position} ${(!this.closed) ? 'open' : 'closed'}" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+          ${(this.label !== '')
+            ? html`<p class="label">${this.label}</p>`
+            : ''
+          }
+          <button type="button" class="close" aria-label="Close" @click=${this._clickClose} tabindex=${ifDefined(tab)}>${this.closeChar}</button>
+        </div>
+        <div class="toast-body">
+          <slot name="head"></slot>
+          <slot name="teaser"></slot>
+          ${(this._hasBody)
+            ? html`
+                ${(this.expanded)
+                  ? html`<slot name="body"></slot>`
+                  : html`<button class="more" @click=${this._clickExpand} tabindex=${ifDefined(tab)}>${this.moreTxt}</button>`
+                }`
+            : ''
+          }
+        </div>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'pop-toast': MyElement
+  }
+}
